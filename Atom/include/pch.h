00e97c1175cffb83ac15debb98fd48618dde6193
帅朝 王 <2410111241@qq.com>
2025-03-12 21:58:27 +0800

    update framework

--- Atom/include/pch.h
+++ Atom/include/pch.h
@@ -1,18 +1,48 @@
+
+// Use the C++ standard templated min/max
+#define NOMINMAX
+
+// DirectX apps don't need GDI
+#define NODRAWTEXT
+#define NOGDI
+#define NOBITMAP
+
+// Include <mcx.h> if you need this
+#define NOMCX
+
+// Include <winsvc.h> if you need this
+#define NOSERVICE
+
+// WinHelp is deprecated
+#define NOHELP
+
+#define WIN32_LEAN_AND_MEAN
+#include <wingdi.h>
 #include <DirectXCollision.h>
 #include <DirectXMath.h>
 #include <d3d12.h>
-#include <dxgi1_4.h>
+#include <dxgi1_6.h>
 #include <DirectXPackedVector.h>
 #include <d3dcompiler.h>
-#include <WindowsX.h>
+#include <Windows.h>
 #include <wrl/client.h>
 #include "d3dx12.h"
-#include <vector>
-
-
-#include <array>
-#include <string>
 #include <comdef.h>
-#include <fstream>
+
+
+#include <queue>
+#include <vector>
+#include <array>
 #include <unordered_map>
+
+#include <utility>
+#include <mutex>
+#include <string>
+
+#include <cstdio>
+#include <sstream>
+#include <fstream>
+
+
+#include "d3dUtil.h"
 
