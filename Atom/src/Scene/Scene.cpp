45f2ad7349efb67ad069da219f398bc0236a04e2
帅朝 王 <2410111241@qq.com>
2025-08-17 22:58:00 +0800

    Add entt

---
+++ Atom/src/Scene/Scene.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+#include "Scene.h"
+
+Scene::Scene()
+{
+	entt::entity entity = m_Registry.create()
+}
+
+Scene::~Scene()
+{
+
+}
---
+++ Atom/src/Scene/Scene.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <entt/entt.hpp>
+
+
+
+class Scene
+{
+public:
+	Scene();
+	~Scene();
+
+private:
+	entt::registry m_Registry;
+
+};
